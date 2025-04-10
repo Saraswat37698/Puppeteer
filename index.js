ca4d8a1c437774ac9adc8da6adce4b308cd4b719
Saraswat37698 <jaiswalacharyya37698@hotmail.com>
2025-04-10 21:15:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const puppeteer=require("puppeteer");
-const XLSX=require("xlsx");
-(async()=>{
+const puppeteer = require("puppeteer");
+const XLSX = require("xlsx");
 
-    const browser=await puppeteer.launch({headless: false});
-    const page=await browser.newPage();
-    await page.goto("https://books.toscrape.com/");
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: "/usr/bin/google-chrome", // Required for Render
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
 
-    const grab=await page.evaluate(()=>{
+  const page = await browser.newPage();
+  await page.goto("https://books.toscrape.com/");
 
-        let t=document.querySelectorAll(".row .product_pod");
-        
-        let arr=[];
+  const grab = await page.evaluate(() => {
+    let t = document.querySelectorAll(".row .product_pod");
+    let arr = [];
 
-        t.forEach((element) => 
-        {
-            let p=element.querySelector("h3 a");
-            let price=element.querySelector(".product_price .price_color");
-            let rating=element.querySelector("p");
-            let avail=element.querySelector(".instock.availability");
-            arr.push({
-                    Book_Name: p.innerHTML,
-                    Price: price.innerHTML,
-                    Rating: rating.className.split(" ")[1],
-                    Availibility: avail.innerText.trim(),
-            })
-        });
-        return arr;
+    t.forEach((element) => {
+      let p = element.querySelector("h3 a");
+      let price = element.querySelector(".product_price .price_color");
+      let rating = element.querySelector("p");
+      let avail = element.querySelector(".instock.availability");
 
-    })
-    console.log(grab);
+      arr.push({
+        Book_Name: p.innerHTML,
+        Price: price.innerHTML,
+        Rating: rating.className.split(" ")[1],
+        Availibility: avail.innerText.trim(),
+      });
+    });
 
-    const ws = XLSX.utils.json_to_sheet(grab);       
-    const wb = XLSX.utils.book_new();                 
-    XLSX.utils.book_append_sheet(wb, ws, "Books");    
-    XLSX.writeFile(wb, "books.xlsx");  
-    
-    await browser.close();
+    return arr;
+  });
 
+  console.log(grab);
+
+  const ws = XLSX.utils.json_to_sheet(grab);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "Books");
+  XLSX.writeFile(wb, "books.xlsx");
+
+  await browser.close();
 })();
