f50de20961b1ca7181dba053e752b381bb41415b
Saraswat37698 <jaiswalacharyya37698@hotmail.com>
2025-04-10 19:55:16 +0530

    Yes

---
+++ index.js
@@ -0,0 +1,40 @@
+const puppeteer=require("puppeteer");
+const XLSX=require("xlsx");
+(async()=>{
+
+    const browser=await puppeteer.launch({headless: false});
+    const page=await browser.newPage();
+    await page.goto("https://books.toscrape.com/");
+
+    const grab=await page.evaluate(()=>{
+
+        let t=document.querySelectorAll(".row .product_pod");
+        
+        let arr=[];
+
+        t.forEach((element) => 
+        {
+            let p=element.querySelector("h3 a");
+            let price=element.querySelector(".product_price .price_color");
+            let rating=element.querySelector("p");
+            let avail=element.querySelector(".instock.availability");
+            arr.push({
+                    Book_Name: p.innerHTML,
+                    Price: price.innerHTML,
+                    Rating: rating.className.split(" ")[1],
+                    Availibility: avail.innerText.trim(),
+            })
+        });
+        return arr;
+
+    })
+    console.log(grab);
+
+    const ws = XLSX.utils.json_to_sheet(grab);       
+    const wb = XLSX.utils.book_new();                 
+    XLSX.utils.book_append_sheet(wb, ws, "Books");    
+    XLSX.writeFile(wb, "books.xlsx");  
+    
+    await browser.close();
+
+})();
